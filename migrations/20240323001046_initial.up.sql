5f1330061a0336d5a23eeb460fc69e4aa5214f5b
Yurb <yavurb@gmail.com>
2024-03-23 14:37:54 -0400

    🐛 fix: remove backticks from migrations

--- migrations/20240323001046_initial.up.sql
+++ migrations/20240323001046_initial.up.sql
@@ -13,17 +13,17 @@ CREATE TABLE posts (
   updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE `projects` (
-  `id` SERIAL PRIMARY KEY,
-  `public_id` VARCHAR(15) NOT NULL UNIQUE,
-  `name` VARCHAR(32) NOT NULL,
-  `description` VARCHAR(255) NOT NULL,
-  `tags` VARCHAR[] NOT NULL,
-  `thumbnail_url` VARCHAR(128) NOT NULL,
-  `website_url` VARCHAR(128) NOT NULL,
-  `live` BOOLEAN NOT NULL DEFAULT FALSE,
-  `created_at` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `updated_at` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `post_id` INTEGER,
+CREATE TABLE projects (
+  id SERIAL PRIMARY KEY,
+  public_id VARCHAR(15) NOT NULL UNIQUE,
+  name VARCHAR(32) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  tags VARCHAR[] NOT NULL,
+  thumbnail_url VARCHAR(128) NOT NULL,
+  website_url VARCHAR(128) NOT NULL,
+  live BOOLEAN NOT NULL DEFAULT FALSE,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  post_id INTEGER,
   FOREIGN KEY (post_id) REFERENCES posts (id)
 );
