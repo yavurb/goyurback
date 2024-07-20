6f9e34ce038f5e7d32e9802bbba4c6dfd7a42408
Yurb <yavurb@gmail.com>
2024-07-20 00:14:30 -0400

    :sparkles: feat: add key validation middleware

--- internal/auth/infrastructure/repository/postgres.go
+++ internal/auth/infrastructure/repository/postgres.go
@@ -2,8 +2,10 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"log"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/yavurb/goyurback/internal/auth/domain"
 	"github.com/yavurb/goyurback/internal/database/postgres"
@@ -52,6 +54,32 @@ func (r *Repository) CreateAPIKey(ctx context.Context, apiKey *domain.APIKeyCrea
 	}
 
 	return newApiKey, nil
+}
+
+func (r *Repository) GetAPIKeyByValue(ctx context.Context, key string) (*domain.APIKey, error) {
+	apiKey_, err := r.db.GetAPIKeyByValue(ctx, key)
+	if err != nil {
+		log.Printf("Error getting APIKey. Got: %v", err)
+
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, domain.ErrAPIKeyNotFound
+		}
+
+		log.Panicf("DB Error obtaining APIKey: %v", err)
+	}
+
+	apiKey := &domain.APIKey{
+		ID:        apiKey_.ID,
+		PublicID:  apiKey_.PublicID,
+		Name:      apiKey_.Name,
+		Key:       apiKey_.Key,
+		Revoked:   apiKey_.Revoked,
+		CreatedAt: apiKey_.CreatedAt.Time,
+		UpdatedAt: apiKey_.UpdatedAt.Time,
+		RevokedAt: apiKey_.RevokedAt.Time,
+	}
+
+	return apiKey, nil
 }
 
 func (r *Repository) RevokeAPIKey(ctx context.Context, publicID string) error {
