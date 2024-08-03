9c0f170b3bbe32fedb7e5f6d8fa71a45df3e64e7
Yurb <yavurb@gmail.com>
2024-08-03 11:18:35 -0400

    :white_check_mark: test: add get projects tests

---
+++ internal/projects/application/get_test.go
@@ -0,0 +1,54 @@
+package application
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/yavurb/goyurback/internal/projects/application/mocks"
+	"github.com/yavurb/goyurback/internal/projects/domain"
+)
+
+func TestGetProject(t *testing.T) {
+	want := &domain.Project{
+		ID:           1,
+		PublicID:     "someid",
+		Name:         "Some Project",
+		Description:  "Some Description",
+		Tags:         []string{"tag1", "tag2"},
+		ThumbnailURL: "https://someurl.com/image.jpg",
+		WebsiteURL:   "https://somewebsite.com",
+		Live:         true,
+		PostID:       1,
+	}
+	repo := &mocks.MockProjectsRepository{
+		GetProjectFn: func(ctx context.Context, id string) (*domain.Project, error) {
+			return &domain.Project{
+				ID:           1,
+				PublicID:     id,
+				Name:         want.Name,
+				Description:  want.Description,
+				Tags:         want.Tags,
+				ThumbnailURL: want.ThumbnailURL,
+				WebsiteURL:   want.WebsiteURL,
+				Live:         want.Live,
+				PostID:       want.PostID,
+			}, nil
+		},
+	}
+
+	uc := NewProjectUsecase(repo)
+
+	project, err := uc.Get(context.Background(), want.PublicID)
+	if err != nil {
+		t.Errorf("Expected no error, got: %v", err)
+	}
+
+	if project == nil {
+		t.Errorf("Expected project to be %v, got nil", want)
+	}
+
+	if !reflect.DeepEqual(project, want) {
+		t.Errorf("Expected project to be %v, got: %v", want, project)
+	}
+}
