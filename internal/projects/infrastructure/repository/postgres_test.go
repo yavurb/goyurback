d6250ab7fc9ede044cedc5b8aad07d27582df845
Yurb <yavurb@gmail.com>
2024-08-17 16:41:50 -0400

    :white_check_mark: add get projects tests

--- internal/projects/infrastructure/repository/postgres_test.go
+++ internal/projects/infrastructure/repository/postgres_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"log"
+	"slices"
 	"testing"
 	"time"
 
@@ -118,6 +119,66 @@ func TestPostgresRepository(t *testing.T) {
 			}
 			if !project.Compare(*want) {
 				t.Errorf("GetProject() got = %v, want %v", project, want)
+			}
+		})
+	})
+
+	t.Run("GetProjects", func(t *testing.T) {
+		t.Run("it should return a list of projects", func(t *testing.T) {
+			testhelpers.CleanDatabase(t, ctx, pgContainer.ConnString)
+			want := []*domain.Project{
+				{
+					ID:           1,
+					PublicID:     "pr_18892",
+					Name:         "Some Project",
+					Description:  "Some Description",
+					Tags:         []string{"tag1", "tag2"},
+					ThumbnailURL: "https://someurl.com/image.jpg",
+					WebsiteURL:   "https://somewebsite.com",
+					Live:         true,
+					PostID:       0,
+					CreatedAt:    time.Now().UTC(),
+					UpdatedAt:    time.Now().UTC(),
+				},
+				{
+					ID:           2,
+					PublicID:     "pr_18893",
+					Name:         "Some Project 2",
+					Description:  "Some Description 2",
+					Tags:         []string{"tag3", "tag4"},
+					ThumbnailURL: "https://someurl.com/image2.jpg",
+					WebsiteURL:   "https://somewebsite2.com",
+					Live:         false,
+					PostID:       0,
+					CreatedAt:    time.Now().UTC(),
+					UpdatedAt:    time.Now().UTC(),
+				},
+			}
+			for _, project := range want {
+				_, _ = repo.CreateProject(ctx, &domain.ProjectCreate{
+					PublicID:     project.PublicID,
+					Name:         project.Name,
+					Description:  project.Description,
+					Tags:         project.Tags,
+					ThumbnailURL: project.ThumbnailURL,
+					WebsiteURL:   project.WebsiteURL,
+					Live:         project.Live,
+				})
+			}
+
+			projects, err := repo.GetProjects(ctx)
+			if err != nil {
+				t.Errorf("GetProjects() error = %v, want no error", err)
+			}
+
+			if len(projects) != 2 {
+				t.Errorf("GetProjects() got = %d, want 2", len(projects))
+			}
+			slices.Reverse(want)
+			for i, project := range projects {
+				if !project.Compare(*want[i]) {
+					t.Errorf("GetProjects() got = %v, want %v", project, want[i])
+				}
 			}
 		})
 	})
