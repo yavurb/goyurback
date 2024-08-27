8b854b27198e657e82b334643ffafe8be1b5854b
Yurb <yavurb@gmail.com>
2024-08-27 10:28:20 -0400

    :white_check_mark: test: add update post tests

--- internal/posts/infrastructure/ui/rest_test.go
+++ internal/posts/infrastructure/ui/rest_test.go
@@ -407,3 +407,161 @@ func TestGetPosts(t *testing.T) {
 		}
 	})
 }
+
+func TestUpdatePost(t *testing.T) {
+	e := echo.New()
+	e.Validator = mods.NewAppValidator()
+
+	t.Run("it should return the updated post", func(t *testing.T) {
+		postIn := map[string]any{
+			"status": "published",
+		}
+		want := map[string]any{
+			"id":           "po_12345",
+			"title":        "My test post",
+			"author":       "Roy",
+			"slug":         "my-test-post",
+			"status":       "published",
+			"description":  "Some post description",
+			"content":      "Some post content",
+			"created_at":   time.Now().UTC().Format(time.RFC3339),
+			"updated_at":   time.Now().UTC().Format(time.RFC3339),
+			"published_at": new(time.Time).Format(time.RFC3339),
+		}
+
+		jsonBytes, err := json.Marshal(postIn)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req := httptest.NewRequest(http.MethodPatch, "/posts/:id", strings.NewReader(string(jsonBytes)))
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		c.SetPath("/posts/:id")
+		c.SetParamNames("id")
+		c.SetParamValues("po_12345")
+
+		uc := &mocks.MockPostsUsecase{}
+		uc.UpdateFn = func(ctx context.Context, id string, title, author, slug, description, content *string, status *domain.Status) (*domain.Post, error) {
+			if id != "po_12345" {
+				return nil, domain.ErrPostNotFound
+			}
+
+			createdAt, _ := time.Parse(time.RFC3339, want["created_at"].(string))
+			updatedAt, _ := time.Parse(time.RFC3339, want["updated_at"].(string))
+
+			post := &domain.Post{
+				ID:          1,
+				PublicID:    "po_12345",
+				Title:       "My test post",
+				Author:      "Roy",
+				Slug:        "my-test-post",
+				Status:      domain.Draft,
+				Description: "Some post description",
+				Content:     "Some post content",
+				CreatedAt:   createdAt,
+				UpdatedAt:   updatedAt,
+			}
+
+			if status != nil {
+				post.Status = *status
+			}
+
+			return post, nil
+		}
+
+		h := NewPostsRouter(e, uc)
+
+		err = h.updatePost(c)
+		if err != nil {
+			t.Errorf("Expected no error updating post. Got: %v", err)
+		}
+
+		if rec.Code != http.StatusOK {
+			t.Errorf("Expected http status code to be %d. Got %d", http.StatusOK, rec.Code)
+		}
+
+		got := make(map[string]any)
+		if err = json.Unmarshal(rec.Body.Bytes(), &got); err != nil {
+			t.Errorf("Error unmarshalling response: %v", err)
+		}
+
+		if !testhelpers.CompareMaps(want, got) {
+			t.Errorf("Mismatch updating post:\n%s", cmp.Diff(want, got))
+		}
+	})
+
+	t.Run("It should return a not found error", func(t *testing.T) {
+		postIn := map[string]any{
+			"status": "published",
+		}
+
+		jsonBytes, err := json.Marshal(postIn)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req := httptest.NewRequest(http.MethodPatch, "/posts/:id", strings.NewReader(string(jsonBytes)))
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		c.SetPath("/posts/:id")
+		c.SetParamNames("id")
+		c.SetParamValues("po_12347")
+
+		uc := &mocks.MockPostsUsecase{}
+		uc.UpdateFn = func(ctx context.Context, id string, title, author, slug, description, content *string, status *domain.Status) (*domain.Post, error) {
+			return nil, domain.ErrPostNotFound
+		}
+
+		h := NewPostsRouter(e, uc)
+
+		err = h.updatePost(c)
+		if err == nil {
+			t.Error("Expected error updating post. Got nil")
+		}
+
+		if !errors.Is(err, echo.ErrNotFound) {
+			t.Errorf("Expected error to be a 404 ErrNotFound. Got: %v", err)
+		}
+	})
+
+	t.Run("It should return an internal server error", func(t *testing.T) {
+		postIn := map[string]any{
+			"status": "published",
+		}
+
+		jsonBytes, err := json.Marshal(postIn)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req := httptest.NewRequest(http.MethodPatch, "/posts/:id", strings.NewReader(string(jsonBytes)))
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		c.SetPath("/posts/:id")
+		c.SetParamNames("id")
+		c.SetParamValues("po_12347")
+
+		uc := &mocks.MockPostsUsecase{}
+		uc.UpdateFn = func(ctx context.Context, id string, title, author, slug, description, content *string, status *domain.Status) (*domain.Post, error) {
+			return nil, errors.New("DB Error")
+		}
+
+		h := NewPostsRouter(e, uc)
+
+		err = h.updatePost(c)
+		if err == nil {
+			t.Error("Expected error updating post. Got nil")
+		}
+
+		if !errors.Is(err, echo.ErrInternalServerError) {
+			t.Errorf("Expected error to be a 500 ErrInternalServerError. Got: %v", err)
+		}
+	})
+}
