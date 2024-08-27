2d0e5c35be35e9ab3ef236ae3443529efc8518de
Yurb <yavurb@gmail.com>
2024-08-27 10:05:36 -0400

    :white_check_mark: test: add get posts tests

--- internal/posts/infrastructure/ui/rest_test.go
+++ internal/posts/infrastructure/ui/rest_test.go
@@ -287,122 +287,123 @@ func TestGetPost(t *testing.T) {
 	})
 }
 
-// func TestGetPosts(t *testing.T) {
-// 	e := echo.New()
-//
-// 	t.Run("it should return the posts", func(t *testing.T) {
-// 		req := httptest.NewRequest(http.MethodGet, "/posts", nil)
-// 		rec := httptest.NewRecorder()
-// 		c := e.NewContext(req, rec)
-//
-// 		c.SetPath("/posts")
-//
-// 		uc := &mocks.MockPostsUsecase{}
-// 		h := NewPostsRouter(e, uc)
-//
-// 		want := map[string][]map[string]any{
-// 			"data": {
-// 				{
-// 					"id":            "pr_12345",
-// 					"name":          "test",
-// 					"description":   "Some post description",
-// 					"tags":          []string{"tag1", "tag2", "tag3"},
-// 					"thumbnail_url": "https://example.com/image.jpg",
-// 					"website_url":   "https://example.com",
-// 					"live":          true,
-// 					"created_at":    time.Now().UTC().Format(time.RFC3339Nano),
-// 					"updated_at":    time.Now().UTC().Format(time.RFC3339Nano),
-// 				},
-// 				{
-// 					"id":            "pr_12346",
-// 					"name":          "test 2",
-// 					"description":   "Some second post description",
-// 					"tags":          []string{"tag4", "tag5", "tag6"},
-// 					"thumbnail_url": "https://example.com/image2.jpg",
-// 					"website_url":   "https://exampletwo.com",
-// 					"live":          true,
-// 					"created_at":    time.Now().UTC().Format(time.RFC3339Nano),
-// 					"updated_at":    time.Now().UTC().Format(time.RFC3339Nano),
-// 				},
-// 			},
-// 		}
-//
-// 		uc.GetPostsFn = func(ctx context.Context) ([]*domain.Post, error) {
-// 			createdAt, _ := time.Parse(time.RFC3339, want["data"][0]["created_at"].(string))
-// 			updatedAt, _ := time.Parse(time.RFC3339, want["data"][0]["updated_at"].(string))
-// 			createdAt2, _ := time.Parse(time.RFC3339, want["data"][1]["created_at"].(string))
-// 			updatedAt2, _ := time.Parse(time.RFC3339, want["data"][1]["updated_at"].(string))
-//
-// 			return []*domain.Post{
-// 				{
-// 					ID:           1,
-// 					PublicID:     "pr_12345",
-// 					Name:         "test",
-// 					Description:  "Some post description",
-// 					ThumbnailURL: "https://example.com/image.jpg",
-// 					WebsiteURL:   "https://example.com",
-// 					Live:         true,
-// 					Tags:         []string{"tag1", "tag2", "tag3"},
-// 					PostID:       1,
-// 					CreatedAt:    createdAt,
-// 					UpdatedAt:    updatedAt,
-// 				},
-// 				{
-// 					ID:           2,
-// 					PublicID:     "pr_12346",
-// 					Name:         "test 2",
-// 					Description:  "Some second post description",
-// 					ThumbnailURL: "https://example.com/image2.jpg",
-// 					WebsiteURL:   "https://exampletwo.com",
-// 					Live:         true,
-// 					Tags:         []string{"tag4", "tag5", "tag6"},
-// 					PostID:       2,
-// 					CreatedAt:    createdAt2,
-// 					UpdatedAt:    updatedAt2,
-// 				},
-// 			}, nil
-// 		}
-//
-// 		err := h.getPosts(c)
-// 		if err != nil {
-// 			t.Errorf("getPosts() error = %v, want no error", err)
-// 		}
-//
-// 		if rec.Code != http.StatusOK {
-// 			t.Errorf("getPosts() status = %v, want %v", rec.Code, http.StatusOK)
-// 		}
-//
-// 		got := make(map[string]any)
-// 		if err = json.Unmarshal(rec.Body.Bytes(), &got); err != nil {
-// 			t.Errorf("Error unmarshalling response: %s", err)
-// 		}
-//
-// 		if !testhelpers.CompareMaps(want, got) {
-// 			t.Errorf("getPost() mismatch:\n%s", cmp.Diff(want, got))
-// 		}
-// 	})
-//
-// 	t.Run("it should return a internal server error", func(t *testing.T) {
-// 		req := httptest.NewRequest(http.MethodGet, "/posts", nil)
-// 		rec := httptest.NewRecorder()
-// 		c := e.NewContext(req, rec)
-//
-// 		c.SetPath("/posts")
-//
-// 		uc := &mocks.MockPostsUsecase{}
-// 		h := NewPostsRouter(e, uc)
-//
-// 		uc.GetPostsFn = func(ctx context.Context) ([]*domain.Post, error) {
-// 			return nil, errors.New("DB Error")
-// 		}
-//
-// 		err := h.getPosts(c)
-// 		if err == nil {
-// 			t.Error("Got nil gettings posts, want error")
-// 		}
-//
-// 		if !errors.Is(err, echo.ErrInternalServerError) {
-// 			t.Errorf("getPost() error = %v, want %v", err, echo.ErrNotFound)
-// 		}
-// 	})
-// }
+func TestGetPosts(t *testing.T) {
+	e := echo.New()
+	e.Validator = mods.NewAppValidator()
+
+	t.Run("it should return the posts", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/posts", nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		uc := &mocks.MockPostsUsecase{}
+
+		want := map[string][]map[string]any{
+			"data": {
+				{
+					"id":           "po_12345",
+					"title":        "My test post",
+					"author":       "Roy",
+					"slug":         "my-test-post",
+					"status":       "published",
+					"description":  "Some post description",
+					"content":      "Some post content",
+					"created_at":   time.Now().UTC().Format(time.RFC3339),
+					"updated_at":   time.Now().UTC().Format(time.RFC3339),
+					"published_at": time.Now().UTC().Format(time.RFC3339),
+				},
+				{
+					"id":           "po_12346",
+					"title":        "My test post 2",
+					"author":       "Roy 2",
+					"slug":         "my-test-post 2",
+					"status":       "published",
+					"description":  "Some post description 2",
+					"content":      "Some post content 2",
+					"created_at":   time.Now().UTC().Format(time.RFC3339),
+					"updated_at":   time.Now().UTC().Format(time.RFC3339),
+					"published_at": time.Now().UTC().Format(time.RFC3339),
+				},
+			},
+		}
+
+		uc.GetPostsFn = func(ctx context.Context) ([]*domain.Post, error) {
+			createdAt, _ := time.Parse(time.RFC3339, want["data"][0]["created_at"].(string))
+			updatedAt, _ := time.Parse(time.RFC3339, want["data"][0]["updated_at"].(string))
+			publishedAt, _ := time.Parse(time.RFC3339, want["data"][0]["published_at"].(string))
+			createdAt2, _ := time.Parse(time.RFC3339, want["data"][1]["created_at"].(string))
+			updatedAt2, _ := time.Parse(time.RFC3339, want["data"][1]["updated_at"].(string))
+			publishedAt2, _ := time.Parse(time.RFC3339, want["data"][1]["published_at"].(string))
+
+			return []*domain.Post{
+				{
+					ID:          1,
+					PublicID:    "po_12345",
+					Title:       "My test post",
+					Author:      "Roy",
+					Slug:        "my-test-post",
+					Status:      domain.Published,
+					Description: "Some post description",
+					Content:     "Some post content",
+					CreatedAt:   createdAt,
+					UpdatedAt:   updatedAt,
+					PublishedAt: publishedAt,
+				},
+				{
+					ID:          2,
+					PublicID:    "po_12346",
+					Title:       "My test post 2",
+					Author:      "Roy 2",
+					Slug:        "my-test-post 2",
+					Status:      domain.Published,
+					Description: "Some post description 2",
+					Content:     "Some post content 2",
+					CreatedAt:   createdAt2,
+					UpdatedAt:   updatedAt2,
+					PublishedAt: publishedAt2,
+				},
+			}, nil
+		}
+
+		h := NewPostsRouter(e, uc)
+
+		err := h.getPosts(c)
+		if err != nil {
+			t.Errorf("Expected no errors getting posts. Got: %v", err)
+		}
+
+		if rec.Code != http.StatusOK {
+			t.Errorf("Expected http status to be a 200 StatusOK. Got: %d", rec.Code)
+		}
+
+		got := make(map[string]any)
+		if err = json.Unmarshal(rec.Body.Bytes(), &got); err != nil {
+			t.Errorf("Error unmarshalling response: %s", err)
+		}
+
+		if !testhelpers.CompareMaps(want, got) {
+			t.Errorf("Mismatch gettings posts:\n%s", cmp.Diff(want, got))
+		}
+	})
+
+	t.Run("it should return a internal server error", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/posts", nil)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		uc := &mocks.MockPostsUsecase{}
+		h := NewPostsRouter(e, uc)
+
+		uc.GetPostsFn = func(ctx context.Context) ([]*domain.Post, error) {
+			return nil, errors.New("DB Error")
+		}
+
+		err := h.getPosts(c)
+		if err == nil {
+			t.Error("Got nil gettings posts, want error")
+		}
+
+		if !errors.Is(err, echo.ErrInternalServerError) {
+			t.Errorf("Expected error to be a 500 Internal Server Error. Got: %v", err)
+		}
+	})
+}
